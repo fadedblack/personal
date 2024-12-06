99568dc6f281ced297b7c5217bbbfa7757ed6c90
Inkeet C <inkeetcha234@gmail.com>
2024-12-06 11:43:59 +0530

    wrote logic for multiplying two numbers

--- multiply.js
+++ multiply.js
@@ -1,7 +1,20 @@
+function getPowerOf2(number) {
+  if (number === 1) {
+    return 0;
+  }
+
+  return 1 + getPowerOf2(number >> 1);
+}
+
 function multiply(number1, number2) {
-  // doing for even
   const min = number1 < number2 ? number1 : number2;
   const max = number1 > number2 ? number1 : number2;
 
-  return max << (min >> 1);
+  const powerOf2 = 2 << (getPowerOf2(max) - 1);
+
+  if (powerOf2 === max) {
+    return min << getPowerOf2(max);
+  }
+
+  return multiply(min, powerOf2) + multiply(min, max - powerOf2);
 }
