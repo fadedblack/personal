90b535730ab62671407679ecc95816461f86caa6
faded_black <inkeetcha234@gmail.com>
2024-12-06 15:09:22 +0530

    handled edge case of 1 x 1

--- multiply.js
+++ multiply.js
@@ -12,9 +12,17 @@ function multiply(number1, number2) {
 
   const powerOf2 = 2 << (getPowerOf2(max) - 1);
 
+  if (min === 1) {
+    return max;
+  }
+
   if (powerOf2 === max) {
     return min << getPowerOf2(max);
   }
 
   return multiply(min, powerOf2) + multiply(min, max - powerOf2);
 }
+
+// function generateTable(multiplicant) {
+  
+// }
