7d8fafc58681394e3827d036a7a4d3e9c64412cd
faded_black <inkeetcha234@gmail.com>
2024-12-10 10:23:24 +0530

    changed loop, cleaned up and removed unnecessary stuffs

--- table.js
+++ table.js
@@ -1,24 +1,44 @@
-function getBorder(start, middle, end, columns, length) {
-  const times = Math.ceil(length / 2);
-  const column = '━'.repeat(times) + middle + '━'.repeat(times);
-
-  const startingSegment = start + '━'.repeat(times);
-  const endingSegment = '━'.repeat(times) + end;
-
-  return startingSegment + column.repeat(columns - 1) + endingSegment;
-}
+//************************************TABLE*************************************
+const DASH = '━';
+const BAR = '┃';
+const SPACE = ' ';
 
 function isEven(number) {
-  return (number & 1) !== 1;
+  return (number & 1) === 0;
 }
 
 function insertData(message, size) {
   const totalSpaces = size - (message + '').length;
+  const extraSpaces = isEven(size) ? 0 : 1; // should i use the binary operation
+  // value directly?
 
-  const extraSpaces = isEven(size) ? 0 : 1;
+  return BAR + SPACE.repeat(Math.floor(totalSpaces / 2)) + message +
+    SPACE.repeat(Math.ceil(totalSpaces / 2) + extraSpaces);
+}
 
-  return '┃' + ' '.repeat(Math.floor(totalSpaces / 2)) + message +
-    ' '.repeat(Math.ceil(totalSpaces / 2) + extraSpaces);
+function insertAllData(values, size) {
+  let table = [];
+
+  for (const row of values) {
+    for (const column of row) {
+      table.push(insertData(column, size));
+    }
+
+    table.push('┃\n' + getBorder('┣', '╋', '┫', row.length, size) + '\n');
+  }
+
+  table.pop();
+  return table.join("");
+}
+
+function getBorder(start, middle, end, columns, length) {
+  const times = Math.ceil(length / 2);
+  const column = DASH.repeat(times) + middle + DASH.repeat(times);
+
+  const startingSegment = start + DASH.repeat(times);
+  const endingSegment = DASH.repeat(times) + end;
+
+  return startingSegment + column.repeat(columns - 1) + endingSegment;
 }
 
 function getLargestSize(values) {
@@ -35,54 +55,33 @@ function getLargestSize(values) {
   return longestString.length;
 }
 
-function insertAllData(values, size) {
-  let table = '';
-
-  for (let row = 0; row < values.length - 1; row += 1) {
-    for (let column = 0; column < values[row].length; column += 1) {
-      table += insertData(values[row][column], size);
-    }
-
-    table += '┃\n' + getBorder('┣', '╋', '┫', values[0].length, size) + '\n';
-  }
-
-  return table;
-}
-
 function createTable(values) {
   const size = getLargestSize(values);
-  
-  let table = getBorder('┏', '┳', '┓', values[0].length, size) + '\n';
 
-  table += insertAllData(values, size);
+  const table = getBorder('┏', '┳', '┓', values[0].length, size) + '\n';
+  const bottom = '┃\n' + getBorder('┗', '┻', '┛', values[0].length, size);
 
-  for (let k = 0; k < values[values.length - 1].length; k += 1) {
-    table += insertData(values[values.length - 1][k], size);
-  }
-
-  table += '┃\n' + getBorder('┗', '┻', '┛', values[0].length, size);
-
-  return table;
+  return table + insertAllData(values, size) + bottom;
 }
 
 const values = [
-  ["Multiplier", "Multiplicant", "Product"],
-  [1, 2, 2],
-  [2, 2, 4],
-  [3, 2, 6],
-  [4, 2, 8],
-  [5, 2, 10],
-  [6, 2, 12],
-  [7, 2, 14],
-  [8, 2, 16],
-  [9, 2, 18],
-  [10, 2, 20]
-  // ["Name", "Age", "Designation"],
-  // ["Shalu Jha", 32, "Intern"],
-  // ["Mallika Hajra", 12, "Intern"],
-  // ["Inkeet", 54, '-'],
-  // ["Vineet Jain", Infinity, "Boss"],
-  // ["Adhithiyan MR", 22, "The Legend"]
+  // ["Multiplier", "Multiplicant", "Product"],
+  // [1, 2, 2],
+  // [2, 2, 4],
+  // [3, 2, 6],
+  // [4, 2, 8],
+  // [5, 2, 10],
+  // [6, 2, 12],
+  // [7, 2, 14],
+  // [8, 2, 16],
+  // [9, 2, 18],
+  // [10, 2, 20]
+  ["Name", "Age", "Designation"],
+  ["Shalu Jha", 32, "Intern"],
+  ["Mallika Hajra", 12, "Intern"],
+  ["Inkeet", 54, '-'],
+  ["Vineet Jain", Infinity, "Boss"],
+  ["Adhithiyan MR", 22, "The Legend"]
 ];
 
 console.log((createTable(values)));
