37a2d5acb601f2a58b0cc9cae06d039d08336209
faded_black <inkeetcha234@gmail.com>
2024-12-17 22:32:16 +0530

    found a new approach to find frequency

--- closure/frequency.js
+++ closure/frequency.js
@@ -1,3 +1,5 @@
+//**********************************Approach 1**********************************
+
 const elementOccurences = function ([match, occurred], char) {
   if (char === match) {
     occurred += 1;
@@ -31,6 +33,26 @@ const frequency = function (string) {
   }, []);
 };
 
+//**********************************Approach 2**********************************
+
+const frequency1 = function (string) {
+  return [...string].reduce(function (array, char) {
+    if (!array.flat().includes(char)) {
+      array.push([char, 1]);
+      return array;
+    }
+
+    const row = array.flat().indexOf(char) >> 1;
+    // 1 => 0
+    // 3 => 1
+    // 5 => 2
+    // 7 => 3
+    array[row][1] += 1;
+
+    return array;
+  }, []);
+};
+
 // hello, l => 2
 // ollolo, l => 3
 // ollollo, a => -1
@@ -39,3 +61,5 @@ const frequency = function (string) {
 // console.log(occurencesOfAll('himasai'));
 // console.log(occurencesOfAll([1,2,3,4,5,1]));
 // console.log(occurencesOfAll('gour adhikary'));
+
+console.table(frequency1('hello'));
