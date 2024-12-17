3d4a8bd99018a8974a3618ad88584ae71dc82e11
faded_black <inkeetcha234@gmail.com>
2024-12-17 19:42:58 +0530

    wrote occurences using closure and reduce

---
+++ closure/occurences.js
@@ -0,0 +1,41 @@
+const elementOccurences = function ([match, occurred], char) {
+  if (char === match) {
+    occurred += 1;
+  }
+
+  return [match, occurred];
+};
+
+const occurences = function (string, char) {
+  return [...string].reduce(elementOccurences, [char, 0]);
+};
+
+const getUniqueElements = function (uniqueElements, currentChar) {
+  if (!uniqueElements.includes(currentChar)) {
+    uniqueElements.push(currentChar);
+  }
+  
+  return uniqueElements;
+};
+
+const removeDuplicate = function (string) {
+  return [...string].reduce(getUniqueElements, []);
+};
+
+const occurencesOfAll = function (string) {
+  const uniqueStrings = removeDuplicate(string);
+
+  return uniqueStrings.reduce(function (array, char) {
+    array.push(occurences(string, char));
+    return array;
+  }, []);
+};
+
+// hello, l => 2
+// ollolo, l => 3
+// ollollo, a => -1
+
+// console.log(occurencesOfAll('hello'));
+// console.log(occurencesOfAll('himasai'));
+// console.log(occurencesOfAll([1,2,3,4,5,1]));
+// console.log(occurencesOfAll('gour adhikary'));
