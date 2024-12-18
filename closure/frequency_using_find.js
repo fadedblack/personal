c34a23fd0deb76085c2612c77942011bfba7bc5a
faded_black <inkeetcha234@gmail.com>
2024-12-18 10:41:40 +0530

    created freqency_using_find.js file

---
+++ closure/frequency_using_find.js
@@ -0,0 +1,17 @@
+const insertIntoTable = function (table, element) {
+  const rowOrUndefined = table.find(function (row) {
+    return row[0] === element;
+  });
+
+  if (rowOrUndefined === undefined) {
+    table.push([element, 1]);
+    return table;
+  }
+
+  rowOrUndefined[1] += 1;
+  return table;
+};
+
+const frequency = function (string) {
+  return [...string].reduce(insertIntoTable, []);
+};
