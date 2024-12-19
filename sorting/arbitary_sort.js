150e128da7dab5fb74a9500b290476f75db11d8d
faded_black <inkeetcha234@gmail.com>
2024-12-19 10:51:48 +0530

    changed approach for the problem, used only one sort

--- sorting/arbitary_sort.js
+++ sorting/arbitary_sort.js
@@ -1,30 +1,13 @@
 // [5,2,3,4,1,10,6,7,9,8] => [1,2,3,4,5,6,10,9,8,7]
 
 const sortAroundPivot = function (array, pivot) {
-  array.sort(function (a, b) {
-    return b - a;
-  });
-
-  return array.sort(function (a) {
-    if (a > pivot) {
-      return -1;
+  return array.sort(function (a, b) {
+    if (b > pivot && a > pivot) {
+      return b - a;
     }
 
-    return 1;
-  }).sort(function () {
-    return -1;
+    return a - b;
   });
 };
 
-// console.log(sortAroundPivot([5, 2, 3, 4, 1, 10, 6, 7, 9, 8], 6));
-console.log(sortAroundPivot([5, 2, 3, 4], 2));
-
-// [5,4,3] => []
-
-[5, 4, 3].sort(function (a, b) {
-  console.log('a:', a, 'b:', b);
-  return a - b;
-});
-
-// [4,5,3] => [3,4,5]
-// [5,4,3] => [4,5,3] => 
+console.log(sortAroundPivot([5,2,3,4,1,10,6,7,9,8], 6));
